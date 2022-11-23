a1c295094eb6a7d7141e66606f53355a9189b59a
sebas <cell333666@gmail.com>
2022-11-23 16:07:09 +0100

    Lösung 1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,42 @@ public class App {
     
     public static void main(String[] args) {
      
+
+        Konto konto = new Konto();
+
+        int stand = konto.getZahl();
+        int neuZahl = stand * 2;
+
+        output("Alter Stand:" + stand + " Neuer Stand:" + neuZahl);
+
+
+
+        Konto konto2 = new Konto();
+        
+        int stand2 = konto.getZahl();
+        int neuZahl2 = konto2.getZahl() * 3;
+
+        output("Alter Stand:" + stand2 + " Neuer Stand:" + neuZahl2);
+
+
+
+        Konto konto3 = new Konto();
+
+        int stand3 = konto.getZahl();
+        int neuZahl3 = konto3.getZahl() * 10;
+        output("Alter Stand:" + stand3 + " Neuer Stand:" + neuZahl3); 
+
     }
+
+       
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
 
 
-
+ 
 }
+
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+
+public class Konto {
+
+    private int zahl = 10000;
+    
+
+
+    public int getZahl() {
+        return zahl; 
+    }
+
+    
+
+
+}
